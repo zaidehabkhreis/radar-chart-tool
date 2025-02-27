e514c11f3469f7aa1d4811595fb356ccb8913dad
zaidehabkhreis <zaid.khreis@devoteam.com>
2025-02-27 15:01:25 +0300

    test

--- Radar_Charts/app.py
+++ Radar_Charts/app.py
@@ -33,6 +33,8 @@ storage_client = storage.Client()
 BUCKET_NAME = "new-radar-chart-users"
 USERS_FILE_NAME = "users.json"
 
+CHECK_INTERVAL = 10
+
 data_dict = {}
 pillar_avg_scores_dict = {}
 latest_hash = None
@@ -132,9 +134,12 @@ def calculate_file_hash(file_stream):
 
 def fetch_latest_excel_if_updated():
     global latest_hash, data_dict, pillar_avg_scores_dict, unique_pillars, last_checked_time
-    
+    current_time = time.time()
+    if current_time - last_checked_time < CHECK_INTERVAL:
+        return False
+    last_checked_time = current_time
+
     try:
-        # Always download the file each request
         request = drive_service.files().get_media(fileId=DRIVE_FILE_ID, supportsAllDrives=True)
         file_stream = io.BytesIO()
         downloader = MediaIoBaseDownload(file_stream, request)
@@ -145,8 +150,6 @@ def fetch_latest_excel_if_updated():
         new_hash = calculate_file_hash(file_stream)
         if new_hash == latest_hash:
             return False
-
-        # If hash changed, parse the file again:
         latest_hash = new_hash
 
         file_stream.seek(0)
@@ -172,7 +175,7 @@ def fetch_latest_excel_if_updated():
                 pillars_in_sheet = df['Pillar'].dropna().unique()
                 all_pillars.update(pillars_in_sheet)
 
-        # Replace the global data
+        # Instead of merging, we replace the dictionaries
         data_dict = new_data_dict
         pillar_avg_scores_dict = new_pillar_avg_scores_dict
 
